6cef9576fb9852b07080873466df6e6d2a961b17
abd eldjalil <selmania.a.eldjalil@gmail.com>
2024-09-16 19:35:44 +0100

    reducing environment variables for netlify deploy second try

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,23 +1,23 @@
 "use client";
 import { Categories } from "@/components/home/Categories";
 import { TopSelling } from "@/components/home/TopSelling";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    async function test() {
-      const response = await fetch("/api/test", {
-        method: "GET",
-      });
-      const message = await response.json();
-      if (message) {
-        setLoading(false);
-      }
-    }
-    test();
-  }, []);
-  if (loading) return <div>Loading...</div>;
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   async function test() {
+  //     const response = await fetch("/api/test", {
+  //       method: "GET",
+  //     });
+  //     const message = await response.json();
+  //     if (message) {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   test();
+  // }, []);
+  // if (loading) return <div>Loading...</div>;
   return (
     <div>
       {!loading && (
