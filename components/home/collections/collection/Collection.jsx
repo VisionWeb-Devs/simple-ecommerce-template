6419ef0e9eac99cae645cd25bc964ce05fbafcac
sadjedb <sadjedbougandoura@gmail.com>
2024-10-26 12:26:19 +0100

    pagination for collection

--- components/home/collections/collection/Collection.jsx
+++ components/home/collections/collection/Collection.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { getCategoriesProducts } from "@/lib/firebase";
 import ProductCard from "../../ProductCard";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Collection = async ({ collectionName }) => {
-  const products = await getCategoriesProducts(collectionName);
-  console.log(products);
-  if (products.length === 0)
+  const [products, setProducts] = useState([]);
+  const [displayedProducts, setDisplayedProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const itemsPerPage = 4;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const allProducts = await getCategoriesProducts(collectionName);
+      setProducts(allProducts);
+      setDisplayedProducts(allProducts.slice(0, itemsPerPage));
+    };
+    fetchProducts();
+  }, [collectionName]);
+
+  const handleViewMore = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    const newDisplayedProducts = products.slice(
+      0,
+      displayedProducts.length + itemsPerPage
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    setDisplayedProducts(newDisplayedProducts);
+    setLoading(false);
+  };
+
+  if (displayedProducts.length === 0) {
     return (
       <div className="flex-col h-[70vh] justify-center items-center w-full py-16 mx-auto lg:px-28 md:px-14 px-9 flex space-y-7">
-        <h1 className=" text-3xl font-medium ">
+        <h1 className="text-3xl font-medium">
           No Products in this category :(
         </h1>
         <div>
@@ -24,15 +53,29 @@ export const Collection = async ({ collectionName }) => {
         </div>
       </div>
     );
+  }
+
   return (
     <div className="bg-gray-100 py-16 flex-grow">
       <div className="container mx-auto lg:px-28 md:px-14 px-9">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {products.map((product) => (
+          {displayedProducts.map((product) => (
             <ProductCard key={product.name} {...product} />
           ))}
         </div>
       </div>
+      {displayedProducts.length < products.length && (
+        <div className="mt-4 flex items-center justify-center">
+          <Button
+            variant="default"
+            size="lg"
+            onClick={handleViewMore}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "View More"}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
